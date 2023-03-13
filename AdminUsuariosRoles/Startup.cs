72653e1ad972a70df4bd778cb3c9cb56e6ccdfa3
JUAN OSBALDO ROLDAN VERA <osbaldo1711@gmail.com>
2023-03-13 10:42:59 -0600

    prueba de commit

--- AdminUsuariosRoles/Startup.cs
+++ AdminUsuariosRoles/Startup.cs
@@ -27,7 +27,7 @@ namespace AdminUsuariosRoles
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. 11
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IRolesService, RolesService>();
